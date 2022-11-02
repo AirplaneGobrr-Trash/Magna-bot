48f96843ee54b4d55ff64fe9570813e8d6f66a27
AirplaneGoBrr <mottlcompound@gmail.com>
2022-11-02 10:45:06 -0700

    Added DM user to bump a server after 30 mins

--- modules/checker/index.js
+++ modules/checker/index.js
@@ -8,6 +8,7 @@ async function check() {
     var isReady = false
     if (disClient.isReady()) isReady = true
 
+    // Servers check
     var serversPath = path.join(global.stuff.startDir, "data", "servers")
     var serversFiles = fs.readdirSync(serversPath)
     for (var serverID of serversFiles) {
@@ -50,6 +51,54 @@ async function check() {
                     }
                 }
             }
+        }
+    }
+
+
+    // Users
+    var usersPath = path.join(global.stuff.startDir, "data", "users")
+    var usersFiles = fs.readdirSync(usersPath)
+
+    for (var userID of usersFiles) {
+        console.log(`Checking user: ${userID}`)
+        var userFilePath = path.join(usersPath, userID)
+        var userDB = new dbClass(userFilePath)
+        if (await userDB.has("bumpInfo")) {
+            console.log("Found bump info!")
+            var currentTime = new Date().valueOf()
+            var nextBump = await userDB.get("bumpInfo.nextAvailableBump")
+            var shouldRemind = await userDB.get(`bumpInfo.remind`)
+            var alreadyReminded = await userDB.get(`bumpInfo.reminded`)
+            var userServers = await userDB.get(`bumpInfo.servers`)
+            var realUserID = userID.replace(".json", "")
+
+            if (!shouldRemind) return console.log(`User: ${realUserID}, does not want bump reminder!`)
+            if (alreadyReminded) return console.log(`Already reminded user: ${realUserID} to bump!`)
+
+            if (currentTime >= nextBump) {
+                    console.log(`Ready to bump!`)
+                    //Reminder to bump server!
+                    if (isReady) {
+                        console.log("Client is logged in!")
+
+                        if (bumpChannel) {
+                            //await bumpChannel.send(`Bump the server! ${bumpRoleID}`)
+                            const exampleEmbed = {
+                                color: "8A2CE2",
+                                title: "You can now bump a server again!",
+                                description: 'Check what servers you can go bump! (This will show here what servers you can bump soon)',
+                                timestamp: new Date().toISOString(),
+                                footer: {
+                                    text: 'By: AirplaneGobrr',
+                                    icon_url: 'https://cdn.discordapp.com/avatars/250029754076495874/d3d522482f97c0c69f826206d04070b1.webp?size=40',
+                                },
+                            }
+
+                            await bumpChannel.send({ embeds: [exampleEmbed] })
+                            await userDB.set(`bumpInfo.reminded`, true)
+                        }
+                    }
+                }
         }
     }
 }
