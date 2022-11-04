c26c57aab77404ee59f6cd71fa682420d31f2dc8
AirplaneGoBrr <71111395+AirplanegoBrr@users.noreply.github.com>
2022-11-04 09:16:24 -0700

    Added super admins

--- modules/socketStuff/index.js
+++ modules/socketStuff/index.js
@@ -3,6 +3,11 @@ const cookie = require("cookie")
 
 const loginChecker = new expressLoginClass({}, "STUPID", "JWT_Token")
 
+// Change to MySQL ?
+const superAdmins = [
+    "250029754076495874"
+]
+
 async function start(){
     var eventEmitter = global.stuff.eventEmitter
     eventEmitter.emit("ready")
@@ -17,6 +22,10 @@ async function start(){
         console.log('a new user connected');
         var cookies = cookie.parse(socket.handshake.headers.cookie)
         var check = await loginChecker.checkAuth(cookies.auth)
+        check.superAdmin = false
+        if (superAdmins.includes(check.authInfo?.id)){
+            check.superAdmin = true
+        }
 
         if (check.auth) {
             socket.join("admins");
